4382cc7b3d09624f8ac340baa918efa7ed76b732
We Li <wei.li@gfk.com>
2019-04-03 11:49:55 +0000

    added functions to recognize speech from audio files

--- app/data_io.py
+++ app/data_io.py
@@ -2,10 +2,64 @@
     Data IO module
 """
 
-import speech_recognition as sr
+from speech_recognition import Recognizer
+from speech_recognition import AudioFile, AudioData
+from speech_recognition import RequestError, UnknownValueError
 
 
-audio_file = sr.AudioFile('test.wav')
+def record_audio_from_file(audio_path: str):
+    """
+    captures audio data from a file
 
-recognizer = sr.Recognizer()
-recognizer.recognize_google()
+    Args:
+        audio_path(str): path to the audio file
+
+    Returns:
+        audio: instance of AudioData
+    """
+
+    recognizer = Recognizer()
+    with AudioFile(audio_path) as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.record(source)
+
+    return audio
+
+
+def recognize_speech_from_audio(audio: AudioData, engine: str = "google"):
+    """
+    returns the recognized text
+
+    Args:
+        audio(AudioData): input audio source
+        engine(str, optional): the engine used for recognizing speech, default to "google", should be one of:
+            google, google_cloud, bing, houndify, ibm, sphinx and wit
+
+    Returns:
+        text: recognized text or None
+    """
+
+    text = None
+    recognizer = Recognizer()
+
+    try:
+        if engine == "google":
+            text = recognizer.recognize_google(audio)
+        if engine == "google_cloud":
+            text = recognizer.recognize_google(audio)
+        if engine == "bing":
+            text = recognizer.recognize_google(audio)
+        if engine == "houndify":
+            text = recognizer.recognize_google(audio)
+        if engine == "ibm":
+            text = recognizer.recognize_google(audio)
+        if engine == "sphinx":
+            text = recognizer.recognize_google(audio)
+        if engine == "wit":
+            text = recognizer.recognize_google(audio)
+    except UnknownValueError:
+        print("Recognition engine({engine}) could not understand audio.".format(engine=engine))
+    except RequestError as e:
+        print("Could not request results from Recognition engine({engine}); {err}.".format(engine=engine, err=e))
+
+    return text
---
+++ tests/app/test_data_io.py
@@ -0,0 +1,34 @@
+"""
+    Testing Data IO
+"""
+
+import os
+
+from speech_recognition import AudioData
+
+from app import data_io
+
+
+resource_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../resources/'))
+
+
+def test_record_audio_from_file():
+    """test record_audio_from_file"""
+
+    sample_audio_path = os.path.join(resource_path, 'sample_speech.wav')
+    audio = data_io.record_audio_from_file(sample_audio_path)
+
+    assert isinstance(audio, AudioData)
+
+
+def test_recognize_speech_from_audio():
+    """test recognize_speech_from_audio"""
+
+    sample_audio_path = os.path.join(resource_path, 'sample_speech.wav')
+    audio = data_io.record_audio_from_file(sample_audio_path)
+    text = data_io.recognize_speech_from_audio(audio)
+    text = text.lower()
+
+    expected = "every word and phrase he speaks is true"
+
+    assert text.startswith(expected)
